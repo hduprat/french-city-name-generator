7f7ee24b049639ac61965375d71e8b81ec503f81
hduprat <hugod@bam.tech>
2019-08-25 23:19:38 +0200

    :sparkles: (rules) Create suffix rules

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { generate } from "./src/generator";
 
 function main() {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 50; i++) {
     const word = generate("startRule");
     console.log(word);
   }
--- src/rules.ts
+++ src/rules.ts
@@ -22,30 +22,73 @@ export interface Rule {
   [n: number]: RuleCase;
 }
 
-const firstSyllableSet = ["tor", "tarn", "arb"];
+const firstSyllableSet = ["Tor", "Tr", "Arb", "Omb", "Flor", "Veg", "Ur"];
+const middleSyllableSet = ["oz", "i", "onz", "ilh", "er"];
+const lastSyllableSet = ["ac", "ay", "émont", "ès", "éville"];
+const laSuffixSet = ["Forêt", "Ville", "Belle", "Vieille", "Mine", "Plaine"];
+const lesSuffixSet = ["Roses", "Monts", "Bains", "Champs"];
 
 const startRule: Rule = {
-  95: {
+  70: {
     valueInSet: firstSyllableSet,
     next: "endRule"
   },
+  95: {
+    valueInSet: firstSyllableSet,
+    next: "middleRule"
+  },
   100: {
-    value: "paris"
+    value: "Paris"
+  }
+};
+
+const middleRule: Rule = {
+  100: {
+    valueInSet: middleSyllableSet,
+    next: "endRule"
   }
 };
 
 const endRule: Rule = {
-  50: {
-    value: "ac"
+  5: {
+    valueInSet: lastSyllableSet,
+    next: "stopRule"
   },
   100: {
-    value: "ay"
+    valueInSet: lastSyllableSet
+  }
+};
+
+const stopRule: Rule = {
+  40: {
+    value: "-les-",
+    next: "lesRule"
+  },
+  100: {
+    value: "-la-",
+    next: "laRule"
+  }
+};
+
+const laRule: Rule = {
+  100: {
+    valueInSet: laSuffixSet
+  }
+};
+
+const lesRule: Rule = {
+  100: {
+    valueInSet: lesSuffixSet
   }
 };
 
 export const rules = {
   startRule,
-  endRule
+  middleRule,
+  endRule,
+  laRule,
+  lesRule,
+  stopRule
 };
 
 export type RuleType = keyof typeof rules;
