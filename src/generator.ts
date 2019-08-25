736204aadde7f3f56471cc65a44a1c88ebde0d51
hduprat <hugod@bam.tech>
2019-08-25 23:53:54 +0200

    :zap: (generator) Switch from recursive to loop

--- src/generator.ts
+++ src/generator.ts
@@ -1,24 +1,22 @@
 import { random100, getRuleThresholds, oneOf } from "./utils";
-import { RuleType, rules, hasFixedValue } from "./rules";
+import { RuleType, rules, hasFixedValue, Rule } from "./rules";
 
-export const generate = (
-  ruleType: RuleType,
-  currentValue: string = ""
-): string => {
-  const n = random100();
-  const rule = rules[ruleType];
-  const ruleThresholds = getRuleThresholds(rule);
+export const generate = (initialRuleType: RuleType): string => {
+  let ruleType: RuleType | undefined = initialRuleType;
+  let currentValue = "";
+  do {
+    const n = random100();
+    const rule: Rule = rules[ruleType];
+    const ruleThresholds = getRuleThresholds(rule);
+    const selectedThreshold = ruleThresholds.find(threshold => n <= threshold);
+    const ruleCase = rule[selectedThreshold || ruleThresholds.length - 1];
 
-  const selectedThreshold = ruleThresholds.find(threshold => n <= threshold);
-  const selectedRuleCase = rule[selectedThreshold || ruleThresholds.length - 1];
-
-  let newValue;
-  if (hasFixedValue(selectedRuleCase))
-    newValue = `${currentValue}${selectedRuleCase.value}`;
-  else {
-    newValue = `${currentValue}${oneOf(selectedRuleCase.valueInSet)}`;
-  }
-
-  if (!selectedRuleCase.next) return newValue;
-  return generate(selectedRuleCase.next, newValue);
+    if (hasFixedValue(ruleCase))
+      currentValue = `${currentValue}${ruleCase.value}`;
+    else {
+      currentValue = `${currentValue}${oneOf(ruleCase.valueInSet)}`;
+    }
+    ruleType = ruleCase.next;
+  } while (!!ruleType);
+  return currentValue;
 };
